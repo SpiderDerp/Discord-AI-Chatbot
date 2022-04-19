dbcaef738778484a5fcad97657602fa21e5c9d30
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2022-04-19 12:59:13 -0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Discord-AI-Chatbot
- A Discord Bot that learns from user messages to speak
+ A Discord Bot that learns from user messages to respond to messages. Feel free to contribute!
+
+**Supported Languages:** Python 3.8+
+
+**Libraries:** [Nextcord](https://github.com/nextcord/nextcord), [Chatterbot](https://github.com/gunthercox/ChatterBot)
+
+ ## Setup
+1. ```pip install -r requirements.txt``` - Installs the necessary libraries to run the bot
+2. Go to the [Discord Developer Portal](https://discord.com/developers/) and create a bot and retrieve its token
+3. Go through ```bot.py``` and change some of the variables in order to customize the bot (token, name, etc.)
+4. Run the bot with ```python3 bot.py```
+5. Go to the [Discord Developer Portal](https://discord.com/developers/) and add the bot to a server
+6. Once the bot is added to a server it will create a channel in which you can chat with it
+7. Enjoy!
+
---
+++ bot.py
@@ -0,0 +1,71 @@
+import nextcord
+from nextcord.ext import commands, tasks
+from nextcord.ext.commands import Bot, cooldown, BucketType
+from nextcord.voice_client import VoiceClient
+from nextcord.ext.tasks import loop
+import asyncio
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+import json
+
+
+TOKEN = "token" #insert token here
+intents = nextcord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!') #insert prefix here
+bot = commands.Bot(command_prefix='!', intents=intents)
+command_prefix='!'
+
+chatbot = ChatBot('Insert Name here')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+for i in range(5):
+    trainer.train("chatterbot.corpus.english")
+
+trainer = ListTrainer(chatbot)
+
+@bot.event
+async def on_message(message):
+
+    if message.author.id == client.user.id: #prevents bot from responding to itself
+        return
+    
+    #checks if message is in talk-with-chatbot
+    if message.channel.name == "talk-with-chatbot" and message.author.id != client.user.id:
+        channel = message.channel
+        #Adds message to a json to train
+        with open('trainingwords.json') as f:
+            data = json.load(f)
+            if message.content not in data["words"]:
+            #adds message to json
+                data["words"].append(message.content)
+                trainer.train(data["words"]) #trains bot
+
+        with open('trainingwords.json', 'w') as f:
+            json.dump(data, f)
+        
+        await channel.send(chatbot.get_response(message.content))
+
+        
+
+
+#on server join make a channel called talk-with-chatbot
+@bot.event
+async def on_guild_join(guild):
+    channel = await guild.create_text_channel('talk-with-chatbot')
+    await channel.send('Hello!')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    game = nextcord.Game("status")
+    await bot.change_presence(status=nextcord.Status.online, activity=game)
+
+bot.run(TOKEN) 
